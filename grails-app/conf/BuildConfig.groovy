fe403a2856550dc4ba848459cbdde54ec7a70e4f
steasdal <sam.teasdale@perceptivesoftware.com>
2014-07-01 16:37:18 -0400

    Updating BuildConfig.groovy to pull arduino-serial dependency from Bintray

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,6 +49,9 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
+
+        mavenRepo "http://dl.bintray.com/steasdal/arduino"
+
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
